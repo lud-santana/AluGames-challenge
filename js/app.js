6e59355c723393bfcd3b0bf58f289565c5c10aae
lud-santana <yurisilvasantana2011@hotmail.com>
2024-01-31 07:18:48 -0300

    Introduzir função para alterar status do botão

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
+function alterarStatus(id)
+{ 
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+    let botao = jogoClicado.querySelector('.dashboard__item__button')
 
-
+    alert(nomeJogo.textContent);
+}
